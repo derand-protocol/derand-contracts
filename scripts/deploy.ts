8850477857e595d7eb04f225340f45fa326422c2
Shayan Shiravani <shayan6959@gmail.com>
2024-02-14 17:40:31 +0330

    Refactor deployment scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,34 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
+
+function sleep(milliseconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
 
 async function main() {
 
-  const contract = await ethers.deployContract("DeRandCoordinator", [
-    "19208182243350969302331310254348577926160425291544448432821516256936827443000",
+  const args = [
+    "93503201847067459816865778983521324688116667814772937141130154736249866362126",
     {
-      x: "0x16d2b68efeb9eb76e61c781b4d0ad807f877c912814271ab173cd927e49888e8",
+      x: "0x4182885822fe798509811091b5b6300299deae3d98a771cc179c2ffd11dabebb",
       parity: 1,
     },
     "0x6121A86157E776e4E6b5d7758E752F214Bf691d3"
-  ]);
+  ]
+
+  const contract = await ethers.deployContract("DeRandCoordinator", args);
 
   await contract.deployed();
 
   console.log(
     `contract deployed to ${contract.address}`
   );
+  
+  await sleep(20000);
+
+  await run("verify:verify", {
+    address: contract.address,
+    constructorArguments: args
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
